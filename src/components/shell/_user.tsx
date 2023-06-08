c181506cb06fbb6572c146b6f71bcdfc05a3e04e
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-06-08 07:29:51 +0100

    styling

--- src/components/shell/_user.tsx
+++ src/components/shell/_user.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import React from 'react';
-import { IconChevronRight, IconChevronLeft } from '@tabler/icons-react';
+import { IconChevronRight, IconChevronLeft, IconPower } from '@tabler/icons-react';
 import { UnstyledButton, Group, Avatar, Text, Box, useMantineTheme, rem, Button, Menu } from '@mantine/core';
-import { signIn, useSession } from 'next-auth/react';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export function User() {
   const theme = useMantineTheme();
@@ -21,7 +21,7 @@ export function User() {
         }`,
       }}
     >
-      <UnstyledButton
+       {user.data ? (<UnstyledButton
         sx={{
           display: 'block',
           width: '100%',
@@ -36,10 +36,10 @@ export function User() {
         }}
       >
    
-        {user.data ? (
-              <Menu shadow="md" width={200}>
-              <Menu.Target>
-              <Group>
+       
+        <Menu shadow="md" width={200}>
+        <Menu.Target>
+        <Group>
           <Avatar
             src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80"
             radius="xl"
@@ -65,26 +65,13 @@ export function User() {
               </Menu.Target>
         
               <Menu.Dropdown>
-                <Menu.Label>Application</Menu.Label>
-                <Menu.Item icon={<IconSettings size={14} />}>Settings</Menu.Item>
-                <Menu.Item icon={<IconMessageCircle size={14} />}>Messages</Menu.Item>
-                <Menu.Item icon={<IconPhoto size={14} />}>Gallery</Menu.Item>
-                <Menu.Item
-                  icon={<IconSearch size={14} />}
-                  rightSection={<Text size="xs" color="dimmed">⌘K</Text>}
-                >
-                  Search
-                </Menu.Item>
-        
-                <Menu.Divider />
-        
-                <Menu.Label>Danger zone</Menu.Label>
-                <Menu.Item icon={<IconArrowsLeftRight size={14} />}>Transfer my data</Menu.Item>
-                <Menu.Item color="red" icon={<IconTrash size={14} />}>Delete my account</Menu.Item>
+                <Menu.Item onClick={() =>signOut()}icon={<IconPower size={14} />}>Sign Out</Menu.Item>
+              
+                
               </Menu.Dropdown>
             </Menu>
-        )  : <Button onClick={ ()=> signIn()}>Login</Button>}
-      </UnstyledButton>
+            </UnstyledButton>
+        )  : ( <Button onClick={ ()=> signIn()}>Login</Button>)}
     </Box>
   );
 }
